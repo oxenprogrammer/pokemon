c8ad8b5ac09da8500db8effc790311a3868c80c2
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-18 01:35:12 +0300

    Feat(pokemon): update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,4 @@
-import {
-  Link,
-  Toolbar,
-  Typography,
-  makeStyles,
-} from "@material-ui/core";
+import { Link, Toolbar, Typography, makeStyles } from "@material-ui/core";
 import React, { Fragment, memo } from "react";
 import {
   Redirect,
@@ -13,15 +8,19 @@ import {
 } from "react-router-dom";
 
 import App from "./App";
+import { Footer } from "./Footer";
 import { Pokemon } from "../containers/Pokemon";
-import logo from '../assets/img/logo.png';
+import logo from "../assets/img/logo3.png";
 
 const useStyles = makeStyles(() => ({
+  main: {
+    display: "grid",
+  },
   root: {
     height: "5.938rem",
     display: "grid",
-    borderBottomLeftRadius: '230px 10px',
-    borderBottomRightRadius: '230px 10px',
+    borderBottomLeftRadius: "230px 10px",
+    borderBottomRightRadius: "230px 10px",
     background:
       "radial-gradient(circle, rgba(45,44,45,1) 0%, rgba(9,9,121,1) 0%, rgba(0,0,0,1) 82%)",
   },
@@ -30,10 +29,11 @@ const useStyles = makeStyles(() => ({
     backgroundColor: "#329B99",
   },
   logo: {
-    width: '10%',
-    position: 'absolute',
-    top: '-2rem',
-    left: '1rem',
+    width: "10%",
+    zIndex: 2,
+    position: "absolute",
+    top: "-2rem",
+    left: "1rem",
     "@media(min-width: 1300px)": {
       width: "8%",
     },
@@ -45,12 +45,12 @@ const useStyles = makeStyles(() => ({
     },
   },
   link: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    color: 'orange',
-    fontWeight: 'bolder',
-    fontSize: '2rem',
-  }
+    display: "flex",
+    justifyContent: "flex-end",
+    color: "orange",
+    fontWeight: "bolder",
+    fontSize: "2rem",
+  },
 }));
 
 // eslint-disable-next-line react/display-name
@@ -58,25 +58,26 @@ const Header = memo(() => {
   const classes = useStyles();
   return (
     <Router>
-      <main>
+      <main className={classes.main}>
         <nav className={classes.topToolbar}></nav>
-        
-          <Toolbar className={classes.root}>
-            <Fragment>
-              <img className={classes.logo} src={logo} alt={'logo'}/>
-            </Fragment>
-            <Typography>
-              <Link className={classes.link} href="/">
-                Pokemon
-              </Link>
-            </Typography>
-          </Toolbar>
-        
+
+        <Toolbar className={classes.root}>
+          <Fragment>
+            <img className={classes.logo} src={logo} alt={"logo"} />
+          </Fragment>
+          <Typography>
+            <Link className={classes.link} href="/">
+              Pokemon
+            </Link>
+          </Typography>
+        </Toolbar>
+
         <Switch>
           <Route path="/" exact component={App} />
           <Route path="/pokemon/:name" exact component={Pokemon} />
           <Redirect to={"/"} />
         </Switch>
+        <Footer />
       </main>
     </Router>
   );
