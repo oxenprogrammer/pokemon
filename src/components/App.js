aa58dc9dba56041bbe91e488d00d7572251a3129
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-17 15:39:29 +0300

    Feat(pokemon): update App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import { PokemonList } from "../containers/PokemonList";
 
 const App = () => {
-  return <div className="App">
-    <PokemonList />
-  </div>;
+  return (
+    <div className="App">
+      <PokemonList />
+    </div>
+  );
 };
 
 export default App;
