5213295fd7e5945d81bfdde83473e3b3b20fcbaf
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-17 15:38:50 +0300

    Feat(pokemon): add list of pokemons

--- src/containers/PokemonList.js
+++ src/containers/PokemonList.js
@@ -1,5 +1,83 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
+import { Link } from "react-router-dom";
+import { Search } from "./Search";
+import { Toolbar } from "@material-ui/core";
+import _ from "lodash";
+import { changeFilter } from "../redux/actions";
+import { getAllPokemon } from "../redux/actions";
+
 export const PokemonList = () => {
+  const dispatch = useDispatch();
+  const pokemonList = useSelector((state) => state.pokemonListReducer);
+  const filter = useSelector((state) => state.filterReducer);
+
+  const fetchData = (page = 1) => {
+    dispatch(getAllPokemon(page));
+  };
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [search, setSearch] = useState("");
+
+  const handleSearch = ({ target }) => {
+    setSearch(target.value);
+    dispatch(changeFilter(target.value));
+  };
+
+  const getData = () => {
+    console.log("list", pokemonList);
+    if (pokemonList.loading) {
+      return <p>loading...</p>;
+    }
+    if (!_.isEmpty(pokemonList.data)) {
+      if (filter === "") {
+        return (
+          <>
+            {pokemonList.data.map((element) => (
+              <div key={element.name}>
+                <span>{element.name}</span>
+                <Link to={`/pokemon/${element.name}`}>View Detail</Link>
+              </div>
+            ))}
+          </>
+        );
+      } else {
+        return (
+          <>
+            {pokemonList.data
+              .filter((element) => element.name.toLowerCase().includes(filter))
+              .map((element) => (
+                <div key={element.name}>
+                  <span>{element.name}</span>
+                  <Link to={`/pokemon/${element.name}`}>View Detail</Link>
+                </div>
+              ))}
+          </>
+        );
+      }
+    }
+    if (pokemonList.error !== "") {
+      return <p>error...</p>;
+    }
+    return <p>unable to find data, try later...</p>;
+  };
   return (
-    <div>PokemonList</div>
+    <>
+      <Toolbar>
+        <Search
+          name={search.name}
+          id={search.name}
+          autoComplete="false"
+          label="Search Pokemon . . ."
+          onChange={handleSearch}
+        />
+      </Toolbar>
+      <div>{getData()}</div>
+    </>
   );
-}
+};
