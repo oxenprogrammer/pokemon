8b19ccd0ee6c6d3d8e81844348a383afb38b71ee
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-19 16:06:39 +0300

    Test(pokemon): add Footer should render

---
+++ src/components/__test__/Footer.test.js
@@ -0,0 +1,26 @@
+import { Footer } from "../Footer";
+import React from "react";
+import { act } from "react-dom/test-utils";
+import { render } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
+
+describe("Header Component", () => {
+  let container = null;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  it("should render the Footer", () => {
+    act(() => {
+      render(<Footer />, container);
+    });
+    expect(container).toBeDefined();
+  });
+});
